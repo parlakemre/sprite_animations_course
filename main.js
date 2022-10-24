dc31e2e316edd04e7678235806fb2c2678c5eb1d
Emre <emre.parlak.95@gmail.com>
2022-10-24 13:22:52 +0300

    first way of doing sprite animations. this techinque is not that advanced since i have to change the modulus values manually.

--- main.js
+++ main.js
@@ -5,13 +5,34 @@ const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
 
 const player_image = new Image();
-player_image.scr = "shadow_dog.png";
-console.log("🚀 ~ file: main.js ~ line 9 ~ player_image", player_image);
+player_image.src = "shadow_dog.png";
+const sprite_width = 575;
+const sprite_height = 523;
+let frameX = 0;
+let frameY = 0;
+let game_frame = 0;
+const stagger_frames = 5;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.fillRect(0, 0, 100, 100);
-  ctx.drawImage(player_image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(
+    player_image,
+    frameX * sprite_width,
+    frameY * sprite_height,
+    sprite_width,
+    sprite_height,
+    0,
+    0,
+    sprite_width,
+    sprite_height
+  );
+
+  if (game_frame % stagger_frames == 0) {
+    if (frameX < 6) frameX++;
+    else frameX %= 5;
+  }
+  game_frame++;
   requestAnimationFrame(animate);
 }
+
 animate();
